bbff30ca010b543adb88276f1eff246aad98660c
Nikita Petukhov <nedoumok99@gmail.com>
2017-11-13 20:42:21 -0500

    prettier + tslint

--- src/html.tsx
+++ src/html.tsx
@@ -1,45 +1,55 @@
 /* tslint:disable no-var-requires */
 /* tslint:disable no-console */
 
-import * as React from "react";
+import * as React from 'react'
 
 // Load production style
-let styles: string;
+let styles: string
 if (process.env.NODE_ENV === `production`) {
-    try {
-        styles = require("!raw-loader!../public/styles.css");
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    styles = require('!raw-loader!../public/styles.css')
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 interface HtmlProps {
-    body: any;
-    postBodyComponents: any;
-    headComponents: any;
+  body: any
+  postBodyComponents: any
+  headComponents: any
 }
 
 // Use `module.exports` to be compliant with `webpack-require` import method
 module.exports = React.createClass<HtmlProps, void>({
-    render() {
-        const css = (process.env.NODE_ENV === `production`) ?
-            <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: styles }} />
-            : null;
-        return (
-            <html lang="en">
-                <head>
-                    {this.props.headComponents}
-                    <title>Zip Products, Inc.</title>
-                    <meta charSet="utf8" />
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-                    {css}
-                </head>
-                <body>
-                    <div id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
-                    {this.props.postBodyComponents}
-                </body>
-            </html>
-        )
-    },
-});
+  render() {
+    const css =
+      process.env.NODE_ENV === `production` ? (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: styles }}
+        />
+      ) : null
+    return (
+      <html lang="en">
+        <head>
+          {this.props.headComponents}
+          <title>Zip Products, Inc.</title>
+          <meta charSet="utf8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+          />
+          {css}
+        </head>
+        <body>
+          <div
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+})
